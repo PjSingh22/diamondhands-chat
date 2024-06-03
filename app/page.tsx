660153f509f03c12a32177def8439f43633af778
Prabhjot Singh <pjsingh1359@gmail.com>
2024-06-03 14:49:05 -0700

    add search query

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,100 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { fetchPaginatedData } from "@/lib/utils";
+import { Input } from "@/components/ui/input"
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { filter } from "@chakra-ui/react";
+
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [dataFetched, setDataFetched] = useState(false as boolean);
+  const [chatData, setChatData] = useState([] as Array<any>);
+
+  // console.log(chatData.then((data) => console.log(data)));
+  useEffect(() => {
+    if (!dataFetched) {
+      // const url = new URL("https://665621609f970b3b36c4625e.mockapi.io/users");
+      fetchPaginatedData("https://665621609f970b3b36c4625e.mockapi.io/users", 10, 1).then(
+        (data) => {
+          console.log(data);
+          setChatData(data);
+          setDataFetched(true);
+        }
+      );
+    }
+  }, [dataFetched]);
+
+  const filerUsers = async (term: string) => {
+    if (term.length === 0) {
+      fetchPaginatedData("https://665621609f970b3b36c4625e.mockapi.io/users", 10, 1).then(
+        (data) => {
+          setChatData(data);
+          return;
+        }
+      );
+    }
+    const fetchedUsers = await fetchPaginatedData("https://665621609f970b3b36c4625e.mockapi.io/users", 0, 1).then(data => data);
+    const filteredUsers = fetchedUsers.filter((user: any) => user.username.toLowerCase().includes(term.toLowerCase()));
+
+    setChatData(filteredUsers);
+  };
+
+  const searchUsers = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("search", term);
+    } else {
+      params.delete("search");
+    }
+
+    replace(`${pathname}?${params.toString()}`)
+
+    filerUsers(term);
+  }
+
+
+
+  console.log(chatData);
+  if (!dataFetched) {
+    return <div className="flex justify-center font-extrabold">Loading...</div>;
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="max-w-3xl mx-auto md:py-10 h-screen">
+      <h1 className="text-4xl font-bold text-primary">Chat Engine</h1>
+      <Input
+        onChange={(e => searchUsers(e.target.value))}
+      />
+      <div className="grid grid-cols-1 gap-4 mt-4">
+        {chatData?.map((chat) => (
+          <div
+            key={chat.id}
+            className="bg-card p-4 rounded-lg flex items-center justify-between"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <div className="flex items-center">
+              <Image
+                src={chat.avatar}
+                alt="avatar"
+                width={50}
+                height={50}
+                className="rounded-full"
+              />
+              <div className="ml-4">
+                <h2 className="text-lg font-semibold text-primary">
+                  {chat.username}
+                </h2>
+                {chat.active ? (
+                  <span className="text-sm text-accent text-white">🟢 Active</span>
+                ) : (
+                  <span className="text-sm text-muted text-white">🔴 Offline</span>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function fetchPaginatedData<T>(
+  url: string,
+  limit: number = 10,
+  page: number = 1
+) {
+  const urlObj = new URL(url)
+
+  if (limit >= 10 ) {
+    urlObj.searchParams.append("limit", limit.toString())
+  }
+  urlObj.searchParams.append("page", page.toString())
+  // urlObj.searchParams.append("offset", "2")
+
+  const response = await fetch(urlObj.toString(), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  return response.json()
+}
