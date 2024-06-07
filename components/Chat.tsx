582090e5de7d15af8ae05a7e390faa046a1cb53c
Prabhjot Singh <pjsingh1359@gmail.com>
2024-06-07 12:23:54 -0700

    add unique colors to usernames for messages

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -9,19 +9,19 @@ export default function Chat() {
         {/* add dummy messages */}
         <div className='chat-message'>
           <div className='chat-message-content'>
-            <p className='user-name'>tommyBoy22:</p>
+            <p className='user-name' style={{color: "green"}}>tommyBoy22:</p>
             <p className='user-msg'>Hey, how are you?</p>
           </div>
           <div className='chat-message-content'>
-            <p className='user-name'>james:</p>
+            <p className='user-name' style={{color: "red"}}>james:</p>
             <p className='user-msg'>LETS GOOO</p>
           </div>
           <div className='chat-message-content'>
-            <p className='user-name'>ToTheMoon69:</p>
+            <p className='user-name' style={{color: "blue"}}>ToTheMoon69:</p>
             <p className='user-msg'>Diamond handing since day 1!</p>
           </div>
           <div className='chat-message-content'>
-            <p className='user-name'>DiamondApe:</p>
+            <p className='user-name' style={{color: "yellow"}}>DiamondApe:</p>
             <p className='user-msg'>I just bought 20,000 more shares</p>
           </div>
           <div className='chat-message-content'>
